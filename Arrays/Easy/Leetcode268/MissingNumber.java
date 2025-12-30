d1b05c6ea37c257c56c2d473b185aed69fcbceb7
SohamAgawane <sohamagavane@gmail.com>
2025-12-30 18:52:18 +0530

    init

---
+++ Arrays/Easy/Leetcode268/MissingNumber.java
@@ -0,0 +1,39 @@
+package Leetcode268;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 3, 5};
+        missingNumber(arr);
+        xorMethod(arr);
+    }
+
+    public static void missingNumber(int[] arr) {
+        int n = arr.length;
+
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for(int i=0; i<arr.length; i++) {
+            actualSum += arr[i];
+        }
+
+        System.out.println(expectedSum - actualSum);
+
+    }
+
+
+    public static void xorMethod(int[] arr) {
+        int allXOR = 0;
+
+        for(int i=0; i<=arr.length; i++) {
+            allXOR = allXOR ^ i;
+        }
+
+        for(int num : arr) {
+            allXOR = allXOR ^ num;
+        }
+
+        System.out.println(allXOR);
+    }
+
+}
---
+++ Arrays/Easy/LeftRotation.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class LeftRotation {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+//        rotateArray(3, arr);
+//        System.out.println(Arrays.toString(arr));
+
+        rotateLeft(arr, 3);
+    }
+
+    public static void rotateArray(int d, int[] arr) {
+        int[] temp = new int[d];
+
+        for(int i=0; i<temp.length; i++) {
+            temp[i] = arr[i];
+        }
+
+        for(int i=0; i<arr.length - d; i++) {
+            arr[i] = arr[i + d];
+        }
+
+        for(int i = arr.length - d; i< arr.length; i++) {
+            arr[i] = temp[i - d -1];
+        }
+
+    }
+
+    public static void reverse(int[] arr, int start, int end) {
+        while(start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public static void rotateLeft(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+
+        // Step-1 : reverse a array -
+        reverse(arr, 0, n - 1);
+
+        // Step-2 : reverse first (length - k) numbers -
+        reverse(arr, 0, n - k - 1);
+
+        // Step-3 : Reverse last k numbers -
+        reverse(arr, n - k, n- 1);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
