da617ade0303e14a135a8ece8d27b453a1fa79b6
SohamAgawane <sohamagavane@gmail.com>
2025-12-31 19:15:33 +0530

    add

--- Arrays/Easy/Leetcode1/TwoSum.java
+++ Arrays/Easy/Leetcode1/TwoSum.java
@@ -14,6 +14,17 @@ public class TwoSum {
         System.out.println(Arrays.toString(twoSum(a, 6)));
     }
 
+    // public static int[] twoSum(int[] nums, int target) {
+    //     for(int i=0;i<nums.length;i++) {
+    //         for(int j=i+1; j<nums.length;j++) {
+    //             if(nums[i] + nums[j] == target) {
+    //                 return new int[]{i,j};
+    //             }
+    //         }
+    //     }
+    //     return new int[]{-1,-1};
+    // }
+
     public static int[] twoSum(int[] nums, int target) {
         // Create a HashMap
         Map<Integer, Integer> map = new HashMap<>();
