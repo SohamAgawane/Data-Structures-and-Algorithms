afd4a84cda364d74dc038f9804e1589452281d9e
SohamAgawane <sohamagavane@gmail.com>
2026-01-16 16:50:08 +0530

    move zeroes

---
+++ Arrays/Easy/Leetcode283/MoveZeroes.java
@@ -0,0 +1,20 @@
+package Leetcode283;
+
+public class MoveZeroes {
+    public static void moveZeroes(int[] nums) {
+        int j = 0;
+
+        for(int i=0;i<nums.length; i++) {
+            if(nums[i] != 0) {
+                swap(nums, i, j);
+                j++;
+            }
+        }
+    }
+
+    static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
