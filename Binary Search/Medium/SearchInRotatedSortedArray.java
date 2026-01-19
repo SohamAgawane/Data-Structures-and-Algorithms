6bc2ce02b4d338e83411840e1accb99ae13945ea
SohamAgawane <sohamagavane@gmail.com>
2026-01-19 18:04:06 +0530

    binary search

--- Binary Search/Medium/SearchInRotatedSortedArray.java
+++ Binary Search/Medium/SearchInRotatedSortedArray.java
@@ -4,59 +4,29 @@ public class SearchInRotatedSortedArray {
         System.out.println(search(arr, 6));
     }
 
-    public static int findPivot(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-
-        while(start <= end) {
-            int mid = start + ((end - start) / 2);
-
-            if(mid < end && arr[mid] > arr[mid+1]) {
-                return mid;
-            }
-            if(mid > start && arr[mid] < arr[mid-1]) {
-                return mid-1;
-            }
-            if(arr[start] <= arr[mid]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return -1;
-    }
-
-    static int binarySearch(int[] arr, int target, int start, int end) {
-        while(start <= end) {
-            int mid = start + ((end - start) / 2);
-
-            if(target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
-                start = mid + 1;
-            } else {
-                return mid;
-            }
-        }
-        return -1;
-    }
-
     public static int search(int[] nums, int target) {
-        int pivot = findPivot(nums);
+        int start = 0;
+        int end = nums.length - 1;
 
-        // if you did not find the pivot, ie array is not rotated
-        if(pivot == -1) {
-            // just do normal binary search
-            return binarySearch(nums, target, 0, nums.length-1);
-        }
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
 
-        // if pivot is found, you have found 2 ascending sorted array -
-        if(nums[pivot] == target) {
-            return pivot;
+            if(nums[mid] == target) return mid;
+
+            if(nums[start] <= nums[mid]) {  // left half is sorted
+                if(nums[start] <= target && target <= nums[mid]) {  // target lies in left half so eliminate right
+                    end = mid - 1;
+                } else {  // target lies in right so eliminate left
+                    start = mid + 1;
+                }
+            } else {  // right half is sorted
+                if(nums[mid] <= target && target <= nums[end]) {  // target lies in right so eliminate left
+                    start = mid + 1;
+                } else {  // target lies in left so eliminate right
+                    end = mid - 1;
+                }
+            }
         }
-        if(target >= nums[0]) {
-            return binarySearch(nums, target, 0, pivot-1);
-        }
-        return binarySearch(nums, target, pivot + 1, nums.length-1);
+        return -1;
     }
 }
--- Binary Search/Medium/SearchInRotatedSortedArrayWithDuplicateValues.java
+++ Binary Search/Medium/SearchInRotatedSortedArrayWithDuplicateValues.java
@@ -1,3 +1,39 @@
 public class SearchInRotatedSortedArrayWithDuplicateValues {
-    
+    public static void main(String[] args) {
+        int[] arr = {3,3,3,1,2,2,3,3,3};
+        System.out.println(search(arr, 3));
+        System.out.println(search(arr, 4));
+    }
+
+    public static boolean search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] == target) return true;
+
+            if(nums[start] == nums[mid] && nums[mid] == nums[end]) {
+                start++;
+                end--;
+                continue;  // to repeat the process from start instead of running below steps
+            }
+
+            if(nums[start] <= nums[mid]) {  // left half is sorted
+                if(nums[start] <= target && target <= nums[mid]) {  // target lies in left half so eliminate right
+                    end = mid - 1;
+                } else {  // target lies in right so eliminate left
+                    start = mid + 1;
+                }
+            } else {  // right half is sorted
+                if(nums[mid] <= target && target <= nums[end]) {  // target lies in right so eliminate left
+                    start = mid + 1;
+                } else {  // target lies in left so eliminate right
+                    end = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
 }
