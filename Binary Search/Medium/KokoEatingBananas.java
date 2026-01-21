bc629e48c6b8111d61f3c879f3091fcf148aa0fb
SohamAgawane <sohamagavane@gmail.com>
2026-01-21 20:12:27 +0530

    koko

--- Binary Search/Medium/KokoEatingBananas.java
+++ Binary Search/Medium/KokoEatingBananas.java
@@ -5,34 +5,46 @@ public class KokoEatingBananas {
         System.out.println(minEatingSpeed(piles, h));
     }
 
-    public static int max(int[] arr) {
+    // Calculate the maximum element in the array -
+    public static int maxElement(int[] arr) {
         int max = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] > max) {
                 max = arr[i];
             }
         }
         return max;
     }
 
-    public static int reqTime(int[] arr, int hourly) {
-        int totalHours = 0;
-        for (int i = 0; i < arr.length; i++) {
-            totalHours += (arr[i] + hourly - 1) / hourly; // calculating the ceil
+    // Calculates a reasonable lower bound for eating speed.
+    public static int minElement(int[] arr, int h) {
+        int sum = 0;
+        for(int i=0; i<arr.length; i++) {
+            sum += arr[i];
         }
-        return totalHours;
+        return sum / h;
     }
 
-    public static int minEatingSpeed(int[] piles, int h) {
-        int low = 1;
-        int high = max(piles);
+    // Calculates total hours required to eat all piles -
+    public static long requiredTime(int[] arr, int hourly) {
+        long totalHour = 0;
+        for(int i=0; i<arr.length; i++) {
+            totalHour += (arr[i] + hourly - 1) / hourly;
+        }
+        return totalHour;
+    }
 
-        while (low <= high) {
+
+    public static int minEatingSpeed(int[] piles, int h) {
+        int low = minElement(piles, h);
+        int high = maxElement(piles);
+
+        while(low <= high) {
             int mid = low + (high - low) / 2;
 
-            int totalHours = reqTime(piles, mid);
+            long totalHours = requiredTime(piles, mid);
 
-            if (totalHours <= h) {
+            if(totalHours <= h) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
@@ -40,32 +52,4 @@ public class KokoEatingBananas {
         }
         return low;
     }
-
-
-//    public static int minEatingSpeed(int[] piles, int h) {
-//        int low = 1;
-//        int high = 0;
-//
-//        // finding max element in the array
-//        for(int i=0; i<piles.length; i++) {
-//            high = Math.max(high, piles[i]);
-//        }
-//
-//        while(low <= high) {
-//            int mid = low + (high-low) / 2;
-//
-//            // finding required hours
-//            long hours = 0;
-//            for(int i=0; i< piles.length; i++) {
-//                hours += (piles[i] + mid - 1) / mid;
-//            }
-//
-//            if(hours <= h) {
-//                high = mid - 1;
-//            } else {
-//                low = mid + 1;
-//            }
-//        }
-//        return low;
-//    }
 }
